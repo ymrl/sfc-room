7c6cac655c63c7243a7221c7a5ede3943c10b2a3
ymrl <ymrl@c-fo.com>
2014-04-05 00:48:09 +0900

    Delete some `if RUBY_VERSION < "1.9"`

--- lib/sfc-room/utils.rb
+++ lib/sfc-room/utils.rb
@@ -1,38 +1,19 @@
 #coding:UTF-8
 require 'nkf'
-if RUBY_VERSION < "1.9"
-  require 'jcode' 
-  require 'kconv'
-end
 
 module SFCRoom::Utils
   def self.greek_downcase str
-    if RUBY_VERSION < "1.9"
-      encoding = NKF.guess(str)
-      return Kconv.kconv(Kconv.toutf8(str).tr('Α-Ω','α-ω'),encoding,Kconv::UTF8)
-    else
-      encoding = str.encoding
-      return str.encode(Encoding::UTF_8).tr('Α-Ω','α-ω').encode(encoding)
-    end
+    encoding = str.encoding
+    return str.encode(Encoding::UTF_8).tr('Α-Ω','α-ω').encode(encoding)
   end
   def self.greek_upcase str
-    if RUBY_VERSION < "1.9"
-      encoding = NKF.guess(str)
-      return Kconv.kconv(Kconv.toutf8(str).tr('α-ω','Α-Ω'),encoding,Kconv::UTF8)
-    else
-      encoding = str.encoding
-      return str.encode(Encoding::UTF_8).tr('α-ω','Α-Ω').encode(encoding)
-    end
+    encoding = str.encoding
+    return str.encode(Encoding::UTF_8).tr('α-ω','Α-Ω').encode(encoding)
     str.tr('α-ω','Α-Ω')
   end
   def self.hankaku_zenkaku str
-    if RUBY_VERSION < "1.9"
-      encoding = NKF.guess(str)
-      return Kconv.kconv(NKF::nkf('-Z1 -Ww',str),encoding,Kconv::UTF8)
-    else
-      encoding = str.encoding
-      return NKF::nkf('-Z1 -Ww',str).encode(encoding)
-    end
+    encoding = str.encoding
+    return NKF::nkf('-Z1 -Ww',str).encode(encoding)
   end
 
   def self.convert_for_search str
